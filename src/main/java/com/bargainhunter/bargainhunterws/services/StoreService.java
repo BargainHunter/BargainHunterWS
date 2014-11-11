183bc6798dbea041a155e156d3acad52ef27b9d9
Achilleas Naumidis <anaumidis@gmail.com>
2014-11-11 04:18:59 +0200

    Working on issue #8

--- src/main/java/com/bargainhunter/bargainhunterws/services/StoreService.java
+++ src/main/java/com/bargainhunter/bargainhunterws/services/StoreService.java
@@ -3,6 +3,7 @@ package com.bargainhunter.bargainhunterws.services;
 import com.bargainhunter.bargainhunterws.models.Store;
 import com.bargainhunter.bargainhunterws.repository.IStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class StoreService {
     @RequestMapping(value = "/stores", method = RequestMethod.GET)
     public List<Store> getAllStores() {
         return storeRepository.findAll();
+    }
+
+    @RequestMapping(value = "/stores/{storeId}", method = RequestMethod.GET)
+    public Store getStore(@PathVariable Long storeId) {
+        return storeRepository.findOne(storeId);
     }
 }
