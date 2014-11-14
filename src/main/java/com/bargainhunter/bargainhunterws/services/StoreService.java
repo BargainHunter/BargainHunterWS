ba3a9daf11be7b012d3f3328374f1f9258cd9c1b
Achilleas Naoumidis <anaumidis@gmail.com>
2014-11-14 21:50:29 +0200

    Fix merge conflicts with branch dev-issue-#8

--- src/main/java/com/bargainhunter/bargainhunterws/services/StoreService.java
+++ src/main/java/com/bargainhunter/bargainhunterws/services/StoreService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Created by Achilleas Naoumidis on 11/9/14.
